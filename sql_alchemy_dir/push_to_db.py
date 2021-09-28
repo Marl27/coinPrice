f00008331b8bcddc293e7d2dcf76569f99e24167
Golu <himalya.sahota@gmail.com>
2021-09-28 10:07:26 +0100

    adding individual coins info into coin_data table

--- sql_alchemy_dir/push_to_db.py
+++ sql_alchemy_dir/push_to_db.py
@@ -29,47 +29,43 @@ def populate_coin_list():
         coin = models.CoinList(coin_list_id=count, symbol=f"{symbol['symbol']}")
         models.session.merge(coin)
         models.session.commit()
-        #print(symbol)
+
 
 def populate_coin_data():
-    print("starting - populate_coin_data()")
-    #populate_coin_list()
+    populate_coin_list()
     date_time_format = '%d-%m-%Y %H:%M:%S'
+    timeframe = "1d"
+    count = 0
 
     coin_list = models.session.query(models.CoinList).all()
     for coin in coin_list:
-        #print(coin.symbol)
+        count += 1
         coin_name = coin.symbol
         if coin_name.endswith("USDT"):
-            #print("TRUE")
             print(coin_name)
 
             candlesticks = client.get_historical_klines(
-                coin_name, Client.KLINE_INTERVAL_1DAY, "25 Sep, 2021", "27 Sep, 2021"
+                coin_name, Client.KLINE_INTERVAL_1DAY, "27 Sep, 2021", "27 Sep, 2021"
             )
+            print(Client.KLINE_INTERVAL_1DAY)
             for candlestick in candlesticks:
-                print("%d -- %d" % (candlestick[0]/1000, candlestick[6]/1000))
                 start_time = str(datetime.fromtimestamp(candlestick[0]/1000).strftime(date_time_format))
                 end_time = str(datetime.fromtimestamp(candlestick[6]/1000).strftime(date_time_format))
-                print("%s -- %s" % (start_time, end_time))
-                print(candlestick)
-    #coin_data = models.Coin_data(coin_list_id=count, symbol=f"{symbol['symbol']}")
+                coin_data = models.Coin_data(coin_data_id=count,
+                                             symbol=coin_name,
+                                             interval_id= timeframe,
+                                             open_time = start_time,
+                                             open= candlestick[1],
+                                             high = candlestick[2],
+                                             low = candlestick[3],
+                                             close = candlestick[4],
+                                             volume = candlestick[5],
+                                             close_time = end_time,
+                                             )
+                models.session.merge(coin_data)
+                models.session.commit()
 
-
-'''
-    __tablename__ = 'coin_data'
-    coin_data_id = Column(Integer, primary_key=True)
-    symbol = Column(String(10)) #TEXT [pk, not null, unique]
-    interval_id = Column(String(10)) #int [pk, not null]
-    open_time = Column(String(20)) #TEXT [pk, not null]
-    open = Column(String(20)) #NUMERIC(18,9)
-    high = Column(String(20)) #NUMERIC(18,9)
-    low = Column(String(20)) #NUMERIC(18,9)
-    close = Column(String(20)) #NUMERIC(18,9)
-    volume = Column(String(20)) #TEXT
-    close_time = Column(String(20)) #TEXT
-'''
 populate_coin_data()
 
 
-#populate_coin_list()
+
